536938bd2c8c7a15aaebdbcbcc811446ff067bb4
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-08-10 19:04:25 +0200

    Remove authorization service

--- Api/AuthorizationService/Dtos/UserAccountCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthorizationService.Dtos
-{
-	public class UserAccountCreateDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
-
--- Api/AuthorizationService/Dtos/UserAccountReadDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuthorizationService.Dtos
-{
-	public class UserAccountReadDto
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-    }
-}
-
