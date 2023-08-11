1095b10767a52e0145f311e294fb84cfe5663a2d
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-08-11 21:50:47 +0200

    add angular routing

---
+++ front-end/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'account/login', component: LoginComponent },
+    { path: 'account/register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
