a497d24592fbbace2f58957ad4145a6ee1b6dcd1
Nolo Cloud Solutions <thulomaepa@gmail.com>
2023-08-11 22:18:03 +0200

    restart front-end project to update project version

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,21 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    RegisterComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
